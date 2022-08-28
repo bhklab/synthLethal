b1643523ff8b47a4e23ecd48e35d9747864f7d04
Ian Smith <iamsinht@gmail.com>
2022-08-28 08:01:19 -0400

    Download depmap function

---
+++ scripts/downloading_data/depmap_download.R
@@ -0,0 +1,10 @@
+# Downloading appropriate files from depmap.org
+
+wget https://ndownloader.figshare.com/files/34990036 -O CRISPR_gene_effect.csv
+wget https://ndownloader.figshare.com/files/34990033 -O CRISPR_gene_dependency.csv
+wget https://ndownloader.figshare.com/files/34989919 -O CCLE_expression.csv
+wget https://ndownloader.figshare.com/files/34989937 -O CCLE_gene_cn.csv
+wget https://ndownloader.figshare.com/files/34990018 -O CCLE_wes_gene_cn.csv
+wget https://ndownloader.figshare.com/files/34989940 -O CCLE_mutations.csv
+wget https://ndownloader.figshare.com/files/35020903 -O sample_info.csv
+wget https://ndownloader.figshare.com/files/34989901 -O Achilles_metadata.csv
